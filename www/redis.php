1dad9e5594ec94618209e88b862cecd9cca32cee
yoheis <ys@MacBook-Pro.local>
2016-12-05 22:04:46 +0900

    misc

--- www/redis.php
+++ www/redis.php
@@ -1,5 +1,8 @@
 <?php
-$redis = new Redis();
-$redis->connect("127.0.0.1",6379);
-
-echo $redis->ping();
+    $redis = new Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $redis->set('key', 'value');
+    echo 'key:' . $redis->get('key') . '<br/>';
+    $redis->delete('key');
+    echo 'key:' . $redis->get('key') . '<br/>';
+    $redis->close();
